51a9573ec7789636cfb065c8fa6277d95dc75853
Luis Lopez <luigibyte@gmail.com>
2018-06-01 19:13:11 -0300

    add listitem with switch action

--- src/components/List.js
+++ src/components/List.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import Layout from './Layout'
 import { Grid, List, ListItem, ListItemText,
-  ListItemSecondaryAction, IconButton } from '@material-ui/core'
+  ListItemSecondaryAction, IconButton, Switch } from '@material-ui/core'
 import ArrowIcon from '@material-ui/icons/KeyboardArrowRight'
 import LampIcon from '@material-ui/icons/WbIncandescent'
 import { Link } from 'react-router-dom'
 
 const ListView = (props) =>
   <Layout>
-    <Grid item xs={12} sm={12} md={12}>
+    <Grid item xs={12} sm={8} md={6} lg={4}>
       <List>
+        <ListItem key={123} dense>
+          <LampIcon />
+          <ListItemText
+            primary='Living Room Lamp'
+            secondary='ON'
+          />
+          <ListItemSecondaryAction>
+            <Switch />
+          </ListItemSecondaryAction>
+        </ListItem>
         {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(value => (
           <ListItem key={value} dense>
             <LampIcon />
