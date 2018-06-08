5c3993981bd93a793ecb7a6726e4096f55da866f
Luis Lopez <luigibyte@gmail.com>
2018-06-07 23:02:37 -0300

    ListItemSwitch connected to store :)

--- src/components/ListItemSwitch.js
+++ src/components/ListItemSwitch.js
@@ -2,17 +2,25 @@ import React from 'react'
 import { ListItem as ListItem2, ListItemText,
   ListItemSecondaryAction, Switch } from '@material-ui/core'
 import LampIcon from '@material-ui/icons/WbIncandescent'
+import { updateItem } from '../actions'
+import store from '../store'
 
-const ListItemSwitch = ({id, name, state}) =>
+const ListItemSwitch = (props) =>
   <ListItem2 dense>
     <LampIcon />
     <ListItemText
-      primary={name}
-      secondary={state.value}
+      primary={props.name}
+      secondary={props.state.value}
     />
     <ListItemSecondaryAction>
-      <Switch />
+      <Switch checked={props.state.value === 'on'} onChange={(event, value) => {
+        console.log(value)
+        store.dispatch(updateItem({
+          ...props,
+          state: {value: value ? 'on':'off'}
+        }))
+      }}/>
     </ListItemSecondaryAction>
   </ListItem2>
-  
+
 export default ListItemSwitch
